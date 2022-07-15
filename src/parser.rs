7ffd0bc5712bf80eca59942f0f52fa1ea0b60f62
currypseudo <currypseudo@gmail.com>
2022-07-16 01:17:06 +0800

    feat: last_expr error check

--- src/parser.rs
+++ src/parser.rs
@@ -2,7 +2,8 @@ use chumsky::prelude::*;
 
 #[derive(Debug)]
 pub struct Program {
-    exprs: Vec<Expression>,
+    pub exprs: Vec<Expression>,
+    pub last_expr: Expression,
 }
 
 const EXTENDED_IDENTIFIER_CHARS: &str = "!$%&*+-/:<=>?@^_~";
@@ -27,14 +28,9 @@ pub fn parser() -> impl Parser<char, Program, Error = Simple<char>> {
             .padded()
             .labelled("number");
         let ident =
-            filter(|c: &char| c.is_ascii_alphabetic() || EXTENDED_IDENTIFIER_CHARS.contains(*c))
-                .map(Some)
-                .chain::<char, _, _>(
-                    filter(|c: &char| {
-                        c.is_ascii_alphanumeric() || EXTENDED_IDENTIFIER_CHARS.contains(*c)
-                    })
-                    .repeated(),
-                )
+            filter(|c: &char| c.is_ascii_alphanumeric() || EXTENDED_IDENTIFIER_CHARS.contains(*c))
+                .repeated()
+                .at_least(1)
                 .collect()
                 .padded()
                 .map(Expression::Ident)
@@ -48,11 +44,22 @@ pub fn parser() -> impl Parser<char, Program, Error = Simple<char>> {
             .labelled("list");
         num.or(ident).or(list)
     })
+    .padded_by(comment.repeated())
+    .padded()
     .labelled("expression");
-    expr.padded_by(comment.repeated())
-        .padded()
+    expr.clone()
         .repeated()
-        .map(|exprs| Program { exprs })
+        .at_least(1)
+        .map(|exprs| {
+            let len = exprs.len();
+            let mut iter = exprs.into_iter();
+            let mut exprs = Vec::new();
+            for _ in 0..len - 1 {
+                exprs.push(iter.next().unwrap());
+            }
+            let last_expr = iter.next().unwrap();
+            Program { exprs, last_expr }
+        })
         .then_ignore(end().recover_with(skip_then_retry_until([])))
 }
 
@@ -61,11 +68,23 @@ mod tests {
     use super::*;
     use manifest_dir_macros::*;
     use pretty_assertions::assert_eq;
-    use std::fs::File;
+    use std::ffi::OsStr;
+    use std::fs::{read_dir, read_to_string, File};
     use std::io::Write;
+    use std::path::Path;
+    fn assert_eq_or_override(path: &Path, content: &str) {
+        if cfg!(feature = "override-test") {
+            let mut file = File::create(path).unwrap();
+            file.write_all(content.as_bytes()).unwrap();
+        }
+        let actual = read_to_string(path)
+            .unwrap_or_else(|_| String::new())
+            .replace('\r', "");
+        assert_eq!(content, &actual)
+    }
     #[test]
-    fn test_parser() {
-        let input = include_str!("test.scm");
+    fn parser_works() {
+        let input = read_to_string(path!("tests/parser.scm")).unwrap();
         let result = parser().parse(input).unwrap_or_else(|e| {
             use std::fmt::Write;
             let mut s = String::new();
@@ -75,13 +94,26 @@ mod tests {
             panic!("{}", s)
         });
         let content = format!("{:#?}", result);
-        if cfg!(feature = "override_test") {
-            let mut file = File::create(path!("src/test.ast")).unwrap();
-            file.write_all(content.as_bytes()).unwrap();
+        assert_eq_or_override(Path::new(path!("tests/parser.ast")), &content);
+    }
+    #[test]
+    fn errors_works() {
+        let dir = path!("tests/error");
+        for entry in read_dir(dir).unwrap() {
+            let path = entry.unwrap().path();
+            if path.extension() != Some(OsStr::new("scm")) {
+                continue;
+            }
+            let input = read_to_string(&path).unwrap();
+            let result = parser().parse(input).unwrap_err();
+            let mut content = String::new();
+            for error in result {
+                use std::fmt::Write;
+                writeln!(content, "{}", error).unwrap();
+            }
+            let mut error_path = path;
+            error_path.set_extension("err");
+            assert_eq_or_override(&error_path, &content);
         }
-        let actual = std::fs::read_to_string(path!("src/test.ast"))
-            .unwrap()
-            .replace('\r', "");
-        assert_eq!(&content, &actual)
     }
 }
